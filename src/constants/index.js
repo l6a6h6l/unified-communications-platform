945c189d5c85f3f442ae86e3384cddaccf4d9026
l6a6h6l <64456111+l6a6h6l@users.noreply.github.com>
2025-07-28 07:06:59 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,19 @@
+export const HERRAMIENTAS = {
+  INCIDENTES: 'incidentes',
+  COMUNICADOS: 'comunicados'
+};
 
+export const EMPRESAS = {
+  INTERDIN: 'INTERDIN S.A.',
+  DINERS: 'DINERS CLUB DEL ECUADOR'
+};
+
+export const IDIOMAS = {
+  ES: 'es',
+  EN: 'en'
+};
+
+export const CREDENCIALES = {
+  USUARIO: 'admin',
+  PASSWORD: '1234'
+};
